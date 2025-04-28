abb96299c8b731220914c1183e3369627257ec88
YunQi572 <YJunGuo20050702@outlook.com>
2025-04-28 19:14:24 +0800

    Initial commit

---
+++ src/main/java/com/example/terravista/TerraVistaApplication.java
@@ -0,0 +1,13 @@
+package com.example.terravista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraVistaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerraVistaApplication.class, args);
+    }
+
+}
